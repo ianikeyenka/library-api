670e928474f655bc47eeccddfee319ddb2e00892
ilyaa <20prorok02@gmail.com>
2024-02-01 17:38:30 +0300

    add util class Constant

---
+++ book-service/src/main/java/com/example/bookservice/util/Constant.java
@@ -0,0 +1,6 @@
+package com.example.bookservice.util;
+
+public class Constant {
+    public static final String BOOK_NOT_FOUND_BY_ID = "Book not found by ID - %s";
+    public static final String BOOK_NOT_FOUND_BY_ISBN = "Book not found by ISBN - %s";
+}
