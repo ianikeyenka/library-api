f5ed8a205a61f586c09f4dc5d586e6f0252c2738
ilyaa <20prorok02@gmail.com>
2024-02-03 02:06:43 +0300

    changed the return type to Optional<BookModel>

--- book-service/src/main/java/com/example/bookservice/repository/BookRepository.java
+++ book-service/src/main/java/com/example/bookservice/repository/BookRepository.java
@@ -4,7 +4,9 @@ import com.example.bookservice.model.BookModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BookRepository extends JpaRepository<BookModel, Long> {
-    BookModel findByIsbn(String isbn);
+    Optional<BookModel> findByIsbn(String isbn);
 }
