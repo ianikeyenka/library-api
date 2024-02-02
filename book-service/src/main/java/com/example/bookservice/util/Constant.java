c396e33e7ca3e6703a2d5430141cd87f31cab098
ilyaa <20prorok02@gmail.com>
2024-02-02 16:15:33 +0300

    add constant for validation

--- book-service/src/main/java/com/example/bookservice/util/Constant.java
+++ book-service/src/main/java/com/example/bookservice/util/Constant.java
@@ -3,4 +3,10 @@ package com.example.bookservice.util;
 public class Constant {
     public static final String BOOK_NOT_FOUND_BY_ID = "Book not found by ID - %s";
     public static final String BOOK_NOT_FOUND_BY_ISBN = "Book not found by ISBN - %s";
+    public static final String FIELD_CANNOT_BE_EMPTY = "Field cannot be empty";
+    public static final String ISBN_FORMAT_REQUIREMENT = "ISBN should be in the format of XXX-XX-XXXXX-XX-X";
+    public static final String NAME_SIZE = "Field must be between 1 and 100 characters";
+    public static final String GENRE_SIZE = "Field must be between 1 and 50 characters";
+    public static final String DESCRIPTION_SIZE = "Field must be between 1 and 500 characters";
+    public static final String AUTHOR_SIZE = "Field must be between 1 and 100 characters";
 }
