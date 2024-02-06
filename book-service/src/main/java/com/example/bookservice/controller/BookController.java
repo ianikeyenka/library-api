1e48908e29c5e097724afcc43ae9a413ec3b627e
ilyaa <20prorok02@gmail.com>
2024-02-06 17:18:27 +0300

    changed PutMapping to PatchMapping

--- book-service/src/main/java/com/example/bookservice/controller/BookController.java
+++ book-service/src/main/java/com/example/bookservice/controller/BookController.java
@@ -10,9 +10,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -54,7 +54,7 @@ public class BookController {
         return new ResponseEntity<>(book, HttpStatus.CREATED);
     }
 
-    @PutMapping("/{id}")
+    @PatchMapping("/{id}")
     public ResponseEntity<BookResponse> updateBook(@PathVariable Long id, @Valid @RequestBody BookResponse bookResponse) {
         log.info("Updating book with id: {}", id);
         BookResponse book = bookService.updateBook(id, bookResponse);
