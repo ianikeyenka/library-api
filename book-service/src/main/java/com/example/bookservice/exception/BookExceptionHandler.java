d2830f4307e40eb84e471d3fcedceb0eeaa1bcf8
ilyaa <20prorok02@gmail.com>
2024-02-06 18:03:57 +0300

    fixed exception

--- book-service/src/main/java/com/example/bookservice/exception/BookExceptionHandler.java
+++ book-service/src/main/java/com/example/bookservice/exception/BookExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.bookservice.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -20,8 +21,8 @@ public class BookExceptionHandler {
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(ResourceNotValidException.class)
-    public ResponseEntity<ErrorResponseInformation> handleValidationException(ResourceNotValidException ex) {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseInformation> handleValidationException(MethodArgumentNotValidException ex) {
         List<String> errors = ex.getBindingResult()
                 .getFieldErrors()
                 .stream()
